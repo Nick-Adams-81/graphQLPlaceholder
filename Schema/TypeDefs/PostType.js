f90735331633ff2140a2d304a5e9fd180eecd64b
Nick Adams <nick.pappas.adams@gmail.com>
2022-12-25 12:15:15 -0600

    added address type tp person type n graphiql

--- Schema/TypeDefs/PostType.js
+++ Schema/TypeDefs/PostType.js
@@ -1,15 +1,15 @@
-const graphql = require('graphql')
-const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean } = graphql
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean } = graphql;
 
 const PostType = new GraphQLObjectType({
-    name: "Post",
-    fields: () => ({
-        id: { type: GraphQLInt },
-        published: { type: GraphQLBoolean },
-        title: { type: GraphQLString },
-        body: { type: GraphQLString },
-        authorId: {type: GraphQLInt}
-    })
-})
+  name: "Post",
+  fields: () => ({
+    id: { type: GraphQLInt },
+    published: { type: GraphQLBoolean },
+    title: { type: GraphQLString },
+    body: { type: GraphQLString },
+    authorId: { type: GraphQLInt },
+  }),
+});
 
-module.exports = PostType
+module.exports = PostType;
---
+++ Schema/index.js
@@ -0,0 +1,34 @@
+const graphql = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLList,
+} = graphql;
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const PersonType = require("./TypeDefs/PersonType");
+const personData = prisma.person.findMany({
+  include: {
+    address: true,
+    posts: true,
+  },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQuery",
+  fields: {
+    getAllPeople: {
+      type: new GraphQLList(PersonType),
+      args: { id: { type: GraphQLInt } },
+      resolve(parent, args) {
+        return personData;
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({ query: RootQuery });

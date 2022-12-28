cd631e91e34427756da12460579c91c0d67c9eef
Nick Adams <nick.pappas.adams@gmail.com>
2022-12-27 18:53:09 -0600

    fixed routes for mutations

--- Schema/index.js
+++ Schema/index.js
@@ -23,7 +23,7 @@ const RootQuery = new GraphQLObjectType({
       description: "Query to get all people in the database",
       resolve: (__parent, __args) => {
         return axios
-          .get(`http://localhost:5000/api/person`)
+          .get(`http://localhost:5000/person`)
           .then((res) => res.data);
       },
     },
@@ -33,7 +33,7 @@ const RootQuery = new GraphQLObjectType({
       description: "Query to get single person in the database by id",
       resolve: (__parent, args) => {
         return axios
-          .get(`http://localhost:5000/api/person/${args.id}`)
+          .get(`http://localhost:5000/person/${args.id}`)
           .then((res) => res.data);
       },
     },
@@ -61,28 +61,28 @@ const Mutation = new GraphQLObjectType({
           .then((res) => res.data);
       },
     },
-    createAddress: {
-      type: AddressType,
-      args: {
-        street_number: { type: GraphQLString },
-        street_name: { type: GraphQLString },
-        city: { type: GraphQLString },
-        state: { type: GraphQLString },
-        zip_code: { type: GraphQLString },
-        personId: { type: GraphQLInt }
-      },
-      resolve: (__parent, args) => {
-        prisma.address.create({
-          street_number: args.street_number,
-          street_name: args.street_name,
-          city: args.city,
-          state: args.state,
-          zip_code: args.zip_code,
-          personId: args.personId
-        })
-        return args
-      }
-    }
+    // createAddress: {
+    //   type: AddressType,
+    //   args: {
+    //     street_number: { type: GraphQLString },
+    //     street_name: { type: GraphQLString },
+    //     city: { type: GraphQLString },
+    //     state: { type: GraphQLString },
+    //     zip_code: { type: GraphQLString },
+    //     personId: { type: GraphQLInt }
+    //   },
+    //   resolve: (__parent, args) => {
+    //     prisma.address.create({
+    //       street_number: args.street_number,
+    //       street_name: args.street_name,
+    //       city: args.city,
+    //       state: args.state,
+    //       zip_code: args.zip_code,
+    //       personId: args.personId
+    //     })
+    //     return args
+    //   }
+    // }
   },
 });
 

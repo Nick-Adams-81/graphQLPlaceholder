47099955f4a9da11f2216d5c88f9724d8ab9749c
Nick Adams <nick.pappas.adams@gmail.com>
2023-01-03 11:40:33 -0600

    added more posts to seed file

--- prisma/PostSeeds.mjs
+++ prisma/PostSeeds.mjs
@@ -1,27 +1,112 @@
 export const PostSeeds = [
-    {
-        title: "test post 1",
-        body: "this is test post one, i'm testing the posts table",
-        authorId: 1
-    },
-    {
-        title: "test post two",
-        body: "continuing to test the post table relationships, this should be user 1s post",
-        authorId: 1
-    },
-    {
-        title: "test post 3",
-        body: "this post is about some random stuff, i like the color green, and i play chess...",
-        authorId: 1
-    },
-    {
-        title: "user 2 first post",
-        body: "this is user 2's first test post",
-        authorId: 2
-    },
-    {
-        title: "more random stuff for user 2",
-        body: "im still testing user 2's posts, this is more random stuff for that goal...",
-        authorId: 2
-    }
-]
+  {
+    title: "test post 1",
+    body: "this is test post one, i'm testing the posts table",
+    authorId: 1,
+  },
+  {
+    title: "test post two",
+    body: "continuing to test the post table relationships, this should be user 1s post",
+    authorId: 1,
+  },
+  {
+    title: "test post 3",
+    body: "this post is about some random stuff, i like the color green, and i play chess...",
+    authorId: 1,
+  },
+  {
+    title: "user 2 first post",
+    body: "this is user 2's first test post",
+    authorId: 2,
+  },
+  {
+    title: "more random stuff for user 2",
+    body: "im still testing user 2's posts, this is more random stuff for that goal...",
+    authorId: 2,
+  },
+  {
+    title: "nick's bio",
+    body: "I'm Nick, a software dev currently based in Dallas, TX, hope you like my graphql project!",
+    authorId: 3,
+  },
+  {
+    title: "Walt's story",
+    body: "I started as a poker dealer, npw i'm a staff software engineer for Audit board and I love my job!",
+    authorId: 4,
+  },
+  {
+    title: "Matt harris, Poker dealer",
+    body: "Matt started off in Sacramento, CA, made his way to vegas, where he's spent the last 12 years working in poker",
+    authorId: 5,
+  },
+  {
+    title: "Being Cap",
+    body: "It's tough being the leader of a super hero group, the best of the best look to you for leadership and it is not an easy life.",
+    authorId: 6,
+  },
+  {
+    title: "My famous saying",
+    body: "Avengers Assemble!!!",
+    authorId: 6,
+  },
+  {
+    title: "Billionare tech boy",
+    body: "My father made all sorts of weapons for the government, that's how we made our money, I will not do the same",
+    authorId: 7,
+  },
+  {
+    title: "Who am I?",
+    body: "I am Iron man",
+    authorId: 7,
+  },
+  {
+    title: "God of thunder",
+    body: "I've watched my planet explode, my brother die multiple times, and my father turn into ash, my life has been tough",
+    authorId: 8,
+  },
+  {
+    title: "tale of two people",
+    body: "Don't make me angry, you won't like what I become",
+    authorId: 9,
+  },
+  {
+    title: "What I say when i'm angry",
+    body: "HULK SMASH!!!!!",
+    authorId: 9,
+  },
+  {
+    title: "I'm in everything",
+    body: "I was born in russia and trained to be an assasin, now i'm a part of the greatest heroes earth has ever assembled",
+    authorId: 10,
+  },
+  {
+    title: "Friendly neighborhood",
+    body: "I got bit by a radioactive spider, now I can crawl up walls and sling webs everywhere, just your friendly neighborhood Spiderman",
+    authorId: 11,
+  },
+  {
+    title: "The dark knight",
+    body: "My parents were killed when I was a child, I vowed to protect my city from then on, I am Batman!",
+    authorId: 12,
+  },
+  {
+    title: "son of Krypton",
+    body: "I was sent here by my parents when I was a baby, right before my home planet was destroyed, I grew up in Kansas and now protect my new home",
+    authorId: 13,
+  },
+  {
+    title: "being Wonder woman",
+    body: "Expectations were set high for me very young, I hope I've lived up to the amazon way",
+    authorId: 14,
+  },
+  {
+    title: "Talking to fish",
+    body: "Being half human and half Atlantian has made me an outcast in both worlds, now I am the  king of the deep and hero to the world",
+    authorId: 15,
+  },
+  {
+    title: "Faster than you",
+    body: "I can move faster than time itself",
+    authorId: 16,
+  },
+];
